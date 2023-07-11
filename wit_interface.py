b8ea0028b6942ef8e19c47fb75c80fca6835b93c
ChaniChalamish <b0527626958@gmail.com>
2023-07-11 17:03:19 +0300

    add base path

--- wit_interface.py
+++ wit_interface.py
@@ -1,14 +1,2 @@
-from wit import Wit
-
-
 class WitInterface:
-    @staticmethod
-    def handle_commands(action, args):
-        if action == "init":
-            Wit.init()
-            return
-        if action == "add":
-            Wit.add()
-            return
-        else:
-            raise ValueError("not valid wit opt")
+    pass
