5e9ead45216c35fa5e9c0eda2b1c163f8cf2fec0
seminar0533131761 <chani0533131761@gmail.com>
2023-07-12 11:28:01 +0300

    handle args

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 import sys
 
 from wit_interface import WitInterface
 
 if __name__ == "__main__":
-    # TODO: handle edge cases
+    if len(sys.argv) == 1:
+        raise ValueError("no command got")
     command = sys.argv[1]
     args = sys.argv[2:]
     WitInterface.handle_commands(command, args)
