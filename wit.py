e3efd12b01b703d4791c09139eaf06873229b42a
ChaniChalamish <117741319+ChaniChalamish@users.noreply.github.com>
2024-03-05 11:01:16 +0200

    Update wit.py

--- wit.py
+++ wit.py
@@ -1,5 +1,5 @@
 import os
-
+from logger import Logger
 from file_handler import FileHandler
 from wit_exception import WitException
 
@@ -15,29 +15,31 @@ class Wit:
 
     @staticmethod
     def init():
-        print("got to init")
-        if Wit.validate_is_wit_repo():
-            # handle nested wits
-            raise WitException("the init repository already exists")
-        else:
-            FileHandler.create_wit()
+        try:
+            print("got to init")
+            if Wit.validate_is_wit_repo():
+                # handle nested wits
+                raise WitException("the init repository already exists")
+            else:
+                FileHandler.create_wit()
+         except WitException as e:
+              Logger.logger.debug(f"{__name__}  {e}")
 
     @staticmethod
     def move_to_staging(path):
+        Logger.logger.debug(f"{__name__} in move_to_staging")
         target_path = os.path.join(FileHandler.base_path, "staging_area")
         FileHandler.copy_item(path, target_path)
 
     @staticmethod
     def add(path):
-        if not Wit.validate_is_wit_repo():
-            raise WitException("no repository exists")
-        # if args[0] == '.':
-        #     full_path = os.path.abspath(os.getcwd())
-        #     print(full_path)
-        # else:
-        #     full_path = FileHandler.validate_path(args[0])
-        #     print(full_path)
-        Wit.move_to_staging(path)
+        try:
+            if not Wit.validate_is_wit_repo():
+                raise WitException("no repository exists")
+            Logger.logger.info(f"{__name__}  add action was activated with {args[0]} files/folders")
+            Wit.move_to_staging(path)
+         except WitException as e:
+            Logger.logger.debug(f"{__name__} {e}")
 
     @staticmethod
     def commit(args):
